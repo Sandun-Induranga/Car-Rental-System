6f49be1e183bc5cec1eba8b691eefc6424f9dbd6
Sandun Induranga <sandutharu40@gmail.com>
2023-02-12 00:17:53 +0530

    Car Controller Created

---
+++ Backend/src/main/java/lk/easy/car_rental/controller/CarController.java
@@ -0,0 +1,16 @@
+package lk.easy.car_rental.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+
+@RestController
+@RequestMapping("/car")
+@CrossOrigin
+public class CarController {
+}
