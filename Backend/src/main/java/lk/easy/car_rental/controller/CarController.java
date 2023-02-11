01caa6226ec1356e418ea230b461e85e68da64a8
Sandun Induranga <sandutharu40@gmail.com>
2023-02-12 00:31:59 +0530

    Customer Registration html

--- Backend/src/main/java/lk/easy/car_rental/controller/CarController.java
+++ Backend/src/main/java/lk/easy/car_rental/controller/CarController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CarController {
 
     @GetMapping
-    public void getAll(){
+    public void getAll() {
         System.out.println("Invoked");
     }
 
