c8e4f73d52e2870202d77dbe6be4c1d052aa5b59
Sandun Induranga <sandutharu40@gmail.com>
2023-02-13 00:43:49 +0530

    Driver Entity Created

---
+++ Backend/src/main/java/lk/easy/car_rental/dto/Driver.java
@@ -0,0 +1,27 @@
+package lk.easy.car_rental.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class Driver {
+    String nic;
+    String license;
+    String name;
+    String address;
+    String contact;
+    String email;
+    String username;
+    String password;
+    String licenseImage;
+}
