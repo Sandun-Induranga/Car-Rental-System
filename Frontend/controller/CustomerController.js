91e48b46f5e2552f474402341b9143f2e9d47698
Sandun Induranga <sandutharu40@gmail.com>
2023-02-12 10:20:15 +0530

    Bind Event For File Chooser

--- Frontend/controller/CustomerController.js
+++ Frontend/controller/CustomerController.js
@@ -1,1 +1,14 @@
+$("#nicImage").on("change", function (e) {
+    var file = e.target.files;
+    console.log("come")
 
+    if (FileReader && file && file.length){
+        console.log("done")
+        var reader = new FileReader();
+        reader.onload = function () {
+            $("#licenseImgContext").css({"background": `url(${reader.result})`,"background-size":"cover", "background-position":"center"})
+        }
+        reader.readAsDataURL(file[0]);
+    }
+
+})
