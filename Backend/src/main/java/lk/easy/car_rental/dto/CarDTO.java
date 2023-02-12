0501640af24537f32b94d9a5f741f50064f1d9aa
Sandun Induranga <sandutharu40@gmail.com>
2023-02-12 20:23:25 +0530

    CarDTO Created

---
+++ Backend/src/main/java/lk/easy/car_rental/dto/CarDTO.java
@@ -0,0 +1,20 @@
+package lk.easy.car_rental.dto;
+
+import java.math.BigDecimal;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+public class CarDTO {
+    String regNum;
+    String type;
+    String color;
+    String brand;
+    String Availability;
+    String transmissionType;
+    int passengers;
+    BigDecimal extraKMPrice;
+    BigDecimal lostDamage;
+    String meterValue;
+}
