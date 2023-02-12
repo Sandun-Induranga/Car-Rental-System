ec8b6a06bfc388153dc474cbe98ee0f2d1186c4b
Sandun Induranga <sandutharu40@gmail.com>
2023-02-12 21:58:57 +0530

    Removed Boilerplate Codes

--- Frontend/controller/CustomerController.js
+++ Frontend/controller/CustomerController.js
@@ -1,43 +1,29 @@
 const baseurl = "http://localhost:8080/easy/";
 
 // Upload NIC Image
-$("#nicImage").on("change", function (e) {
-    var file = e.target.files;
-    console.log("come")
-
-    if (FileReader && file && file.length) {
-        var reader = new FileReader();
-        reader.onload = function () {
-            $("#nicImgContext").css({
-                "background": `url(${reader.result})`,
-                "background-size": "cover",
-                "background-position": "center"
-            })
-        }
-        reader.readAsDataURL(file[0]);
-    }
-
-})
-
+loadSelectedImage("#nicImage");
 
 // Upload License Image
-$("#licenseImage").on("change", function (e) {
-    let file = e.target.files;
-    console.log("come")
+loadSelectedImage("#licenseImage");
 
-    if (FileReader && file && file.length) {
-        let reader = new FileReader();
-        reader.onload = function () {
-            $("#licenseImgContext").css({
-                "background": `url(${reader.result})`,
-                "background-size": "cover",
-                "background-position": "center"
-            })
+function loadSelectedImage(divId) {
+    $(divId).on("change", function (e) {
+        let file = e.target.files;
+
+        if (FileReader && file && file.length) {
+            let reader = new FileReader();
+            reader.onload = function () {
+                $(divId).parent().children(":eq(0)").css({
+                    "background": `url(${reader.result})`,
+                    "background-size": "cover",
+                    "background-position": "center"
+                });
+            }
+            reader.readAsDataURL(file[0]);
         }
-        reader.readAsDataURL(file[0]);
-    }
 
-})
+    });
+}
 
 $("#btnSubmit").on("click", function () {
 
