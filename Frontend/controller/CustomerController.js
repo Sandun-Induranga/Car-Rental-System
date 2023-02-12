d145d1ac4d9bcf0923145b94b686c222c3ace996
Sandun Induranga <sandutharu40@gmail.com>
2023-02-12 10:58:19 +0530

    Ajax Object Created

--- Frontend/controller/CustomerController.js
+++ Frontend/controller/CustomerController.js
@@ -1,12 +1,18 @@
+const baseurl = "http://localhost:8080/easy/";
+
 // Upload NIC Image
 $("#nicImage").on("change", function (e) {
     var file = e.target.files;
     console.log("come")
 
-    if (FileReader && file && file.length){
+    if (FileReader && file && file.length) {
         var reader = new FileReader();
         reader.onload = function () {
-            $("#nicImgContext").css({"background": `url(${reader.result})`,"background-size":"cover", "background-position":"center"})
+            $("#nicImgContext").css({
+                "background": `url(${reader.result})`,
+                "background-size": "cover",
+                "background-position": "center"
+            })
         }
         reader.readAsDataURL(file[0]);
     }
@@ -19,12 +25,26 @@ $("#licenseImage").on("change", function (e) {
     var file = e.target.files;
     console.log("come")
 
-    if (FileReader && file && file.length){
+    if (FileReader && file && file.length) {
         var reader = new FileReader();
         reader.onload = function () {
-            $("#licenseImgContext").css({"background": `url(${reader.result})`,"background-size":"cover", "background-position":"center"})
+            $("#licenseImgContext").css({
+                "background": `url(${reader.result})`,
+                "background-size": "cover",
+                "background-position": "center"
+            })
         }
         reader.readAsDataURL(file[0]);
     }
 
+})
+
+$.ajax({
+    url: baseurl + "customer",
+    method: "post",
+    contentType: false,
+    processData: false,
+    success: function (res) {
+
+    }
 })
