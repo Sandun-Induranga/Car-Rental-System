1240244e25b127cbcd894ac4f26986f8cf845e06
Sandun Induranga <sandutharu40@gmail.com>
2023-02-13 01:11:05 +0530

    Entity User and UserDTO Created

---
+++ Backend/src/main/java/lk/easy/car_rental/dto/UserDTO.java
@@ -0,0 +1,21 @@
+package lk.easy.car_rental.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class UserDTO {
+    String username;
+    String password;
+    String role;
+}
---
+++ Backend/src/main/java/lk/easy/car_rental/entity/User.java
@@ -0,0 +1,21 @@
+package lk.easy.car_rental.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class User {
+    String username;
+    String password;
+    String role;
+}
