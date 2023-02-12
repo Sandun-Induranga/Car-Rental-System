0e75fcb11515697846f5f42b87c7d8319f8ebe0b
Sandun Induranga <sandutharu40@gmail.com>
2023-02-12 12:28:13 +0530

    Customer Controller Updated

--- Backend/src/main/java/lk/easy/car_rental/controller/CarController.java
+++ Backend/src/main/java/lk/easy/car_rental/controller/CarController.java
@@ -1,9 +1,8 @@
 package lk.easy.car_rental.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * @author : Sandun Induranga
@@ -17,7 +16,7 @@ public class CarController {
 
     @GetMapping
     public void getAll() {
-        System.out.println("Invoked");
+
     }
 
 }
--- Backend/src/main/java/lk/easy/car_rental/controller/CustomerController.java
+++ Backend/src/main/java/lk/easy/car_rental/controller/CustomerController.java
@@ -1,6 +1,14 @@
 package lk.easy.car_rental.controller;
 
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * @author : Sandun Induranga
@@ -13,8 +21,24 @@ import org.springframework.web.bind.annotation.*;
 public class CustomerController {
 
     @PostMapping
-    public void getAll(){
-        System.out.println("Invoked");
+    public void getAll(@RequestParam("file") MultipartFile file, ModelMap modelMap){
+
+        modelMap.addAttribute("file", file);
+
+        try {
+            byte[] byteArray = file.getBytes();
+            String serverPath = new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().toURI()).getParentFile().getParentFile().getParentFile().getAbsolutePath();
+
+            Path location = Paths.get(serverPath + "/image.jpeg");
+            Files.write(location, byteArray);
+            file.transferTo(location);
+            System.out.println(serverPath);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
