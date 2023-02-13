6fef268c25912dddab6db9f996bf0439ab38e25f
Sandun Induranga <sandutharu40@gmail.com>
2023-02-13 14:53:52 +0530

    Content Updated in CustomerServiceImpl

--- Backend/src/main/java/lk/easy/car_rental/service/impl/CustomerServiceImpl.java
+++ Backend/src/main/java/lk/easy/car_rental/service/impl/CustomerServiceImpl.java
@@ -9,6 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * @author : Sandun Induranga
  * @since : 0.1.0
@@ -25,9 +32,34 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public void saveCustomer(CustomerDTO customerDTO) throws RuntimeException {
-        if (customerRepo.existsById(customerDTO.getNic())){
-            throw new RuntimeException("Customer Already Exits..!");
+
+        Customer customer = mapper.map(customerDTO, Customer.class);
+
+        if (customerRepo.existsById(customerDTO.getNic())) throw new RuntimeException("Customer Already Exits..!");
+
+        try {
+            byte[] nicFileBytes = customerDTO.getNicImage().getBytes();
+            byte[] licenseFileBytes = customerDTO.getLicenseImage().getBytes();
+
+            String serverPath = new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().toURI()).getParentFile().getParentFile().getParentFile().getAbsolutePath();
+            Path nicLocation = Paths.get(serverPath + "/nic_image.jpeg");
+            Path licenseLocation = Paths.get(serverPath + "/license_image.jpeg");
+
+            Files.write(nicLocation, nicFileBytes);
+            Files.write(licenseLocation, licenseFileBytes);
+
+            customerDTO.getNicImage().transferTo(nicLocation);
+            customerDTO.getLicenseImage().transferTo(licenseLocation);
+
+            customer.setNicImage(nicLocation.toString());
+            customer.setLicenseImage(licenseLocation.toString());
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
         }
-        customerRepo.save(mapper.map(customerDTO, Customer.class));
+
+        customerRepo.save(customer);
     }
 }
