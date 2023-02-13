9c6f57430b5654114db88345743a51b7e88c389a
Sandun Induranga <sandutharu40@gmail.com>
2023-02-13 15:50:49 +0530

    Created Service Classes For Car

---
+++ Backend/src/main/java/lk/easy/car_rental/service/impl/CarServiceImpl.java
@@ -0,0 +1,10 @@
+package lk.easy.car_rental.service.impl;
+
+import lk.easy.car_rental.service.CarService;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+public class CarServiceImpl implements CarService {
+}
