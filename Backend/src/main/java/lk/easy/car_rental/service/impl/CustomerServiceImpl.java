565023236fd07104cde1e7f3bf24244a8ecd0fdd
Sandun Induranga <sandutharu40@gmail.com>
2023-02-13 14:45:36 +0530

    Content Updated in CustomerServiceImpl

--- Backend/src/main/java/lk/easy/car_rental/service/impl/CustomerServiceImpl.java
+++ Backend/src/main/java/lk/easy/car_rental/service/impl/CustomerServiceImpl.java
@@ -1,6 +1,10 @@
 package lk.easy.car_rental.service.impl;
 
+import lk.easy.car_rental.dto.CustomerDTO;
+import lk.easy.car_rental.entity.Customer;
+import lk.easy.car_rental.repo.CustomerRepo;
 import lk.easy.car_rental.service.CustomerService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,8 +18,16 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class CustomerServiceImpl implements CustomerService {
 
-    @Override
-    public void saveCustomer() throws RuntimeException {
+    @Autowired
+    CustomerRepo customerRepo;
 
+    ModelMapper mapper;
+
+    @Override
+    public void saveCustomer(CustomerDTO customerDTO) throws RuntimeException {
+        if (customerRepo.existsById(customerDTO.getNic())){
+            throw new RuntimeException("Customer Already Exits..!");
+        }
+        customerRepo.save(mapper.map(customerDTO, Customer.class));
     }
 }
