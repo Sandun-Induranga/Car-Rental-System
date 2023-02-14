61e851279c1206fb91ce820c5c8bb9300bc6801f
Sandun Induranga <sandutharu40@gmail.com>
2023-02-14 10:34:12 +0530

    ResponseUtil Created

---
+++ Backend/src/main/java/lk/easy/car_rental/util/ResponseUtil.java
@@ -0,0 +1,21 @@
+package lk.easy.car_rental.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class ResponseUtil {
+    String state;
+    String message;
+    Object data;
+}
