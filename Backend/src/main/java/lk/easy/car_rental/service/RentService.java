00ed7f736678489811e852cf9b1e86251ff38c02
Sandun Induranga <sandutharu40@gmail.com>
2023-02-15 16:25:30 +0530

    Created Services For Rent

---
+++ Backend/src/main/java/lk/easy/car_rental/service/RentService.java
@@ -0,0 +1,11 @@
+package lk.easy.car_rental.service;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+public interface RentService {
+
+    public void requestRent() throws RuntimeException;
+
+}
---
+++ Backend/src/main/java/lk/easy/car_rental/service/impl/RentServiceImpl.java
@@ -0,0 +1,16 @@
+package lk.easy.car_rental.service.impl;
+
+import lk.easy.car_rental.service.RentService;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+public class RentServiceImpl implements RentService {
+
+    @Override
+    public void requestRent() throws RuntimeException {
+
+    }
+
+}
