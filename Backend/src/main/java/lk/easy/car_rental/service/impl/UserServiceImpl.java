2b1b74724d6c7111d25933a903b96265f97e5024
Sandun Induranga <sandutharu40@gmail.com>
2023-02-16 23:45:02 +0530

    Created Services For User

---
+++ Backend/src/main/java/lk/easy/car_rental/service/impl/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package lk.easy.car_rental.service.impl;
+
+import lk.easy.car_rental.dto.UserDTO;
+import lk.easy.car_rental.service.UserService;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public UserDTO getUser() throws RuntimeException {
+        return null;
+    }
+
+}
