e1ef9a1a9941b99cbd86bf4001d7d1a3853f1c32
Sandun Induranga <sandutharu40@gmail.com>
2023-02-17 00:10:41 +0530

    Get User

--- Backend/src/main/java/lk/easy/car_rental/service/impl/UserServiceImpl.java
+++ Backend/src/main/java/lk/easy/car_rental/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import lk.easy.car_rental.dto.UserDTO;
 import lk.easy.car_rental.entity.User;
 import lk.easy.car_rental.repo.UserRepo;
 import lk.easy.car_rental.service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,15 +21,16 @@ public class UserServiceImpl implements UserService {
     @Autowired
     UserRepo userRepo;
 
+    ModelMapper mapper;
+
     @Override
     public UserDTO getUser(String username, String password) throws RuntimeException {
 
         User user = userRepo.findById(username).get();
-        if (!user.getPassword().equals(password)) throw new RuntimeException("Wrong Login Details. Please Try Again..!");
+        if (!user.getPassword().equals(password))
+            throw new RuntimeException("Wrong Login Details. Please Try Again..!");
 
-
-
-        return null;
+        return mapper.map(user, UserDTO.class);
 
     }
 
