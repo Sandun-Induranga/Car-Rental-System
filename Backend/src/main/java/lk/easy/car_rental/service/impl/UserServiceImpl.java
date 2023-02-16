fd452779e2bbfe8f476fe8a6dbae89c4b0d8b14f
Sandun Induranga <sandutharu40@gmail.com>
2023-02-16 23:57:31 +0530

    Content Updated

--- Backend/src/main/java/lk/easy/car_rental/service/impl/UserServiceImpl.java
+++ Backend/src/main/java/lk/easy/car_rental/service/impl/UserServiceImpl.java
@@ -1,11 +1,14 @@
 package lk.easy.car_rental.service.impl;
 
 import lk.easy.car_rental.dto.UserDTO;
+import lk.easy.car_rental.entity.User;
 import lk.easy.car_rental.repo.UserRepo;
 import lk.easy.car_rental.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 /**
  * @author : Sandun Induranga
@@ -21,7 +24,13 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDTO getUser(String username) throws RuntimeException {
+
+        User user = userRepo.findById(username).get();
+
+        System.out.println(user);
+
         return null;
+
     }
 
 }
