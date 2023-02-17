8ba046260bc75746bf6ce83405426dbe55b833bd
Sandun Induranga <sandutharu40@gmail.com>
2023-02-18 00:42:16 +0530

    Current User Details

--- Backend/src/main/java/lk/easy/car_rental/controller/ManageLoginController.java
+++ Backend/src/main/java/lk/easy/car_rental/controller/ManageLoginController.java
@@ -1,6 +1,7 @@
 package lk.easy.car_rental.controller;
 
 import lk.easy.car_rental.service.UserService;
+import lk.easy.car_rental.util.CurrentUserUtil;
 import lk.easy.car_rental.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -22,6 +23,13 @@ public class ManageLoginController {
     public ResponseUtil getUser(@RequestParam String username, @RequestParam String password) {
 
         return new ResponseUtil("OK", "Successfully Loaded..!", userService.getUser(username, password));
+
+    }
+
+    @GetMapping
+    public ResponseUtil getCurrentUserDetails() {
+
+        return new ResponseUtil("OK", "Successfully Loaded..!", CurrentUserUtil.currentUser);
 
     }
 
