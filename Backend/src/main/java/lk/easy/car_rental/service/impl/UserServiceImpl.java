6b0764e36df531fb57c277e82848297cc0deb0ce
Sandun Induranga <sandutharu40@gmail.com>
2023-02-18 00:39:52 +0530

    Current User Details

--- Backend/src/main/java/lk/easy/car_rental/service/impl/UserServiceImpl.java
+++ Backend/src/main/java/lk/easy/car_rental/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import lk.easy.car_rental.dto.UserDTO;
 import lk.easy.car_rental.entity.User;
 import lk.easy.car_rental.repo.UserRepo;
 import lk.easy.car_rental.service.UserService;
+import lk.easy.car_rental.util.CurrentUserUtil;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +34,7 @@ public class UserServiceImpl implements UserService {
             throw new RuntimeException("Wrong Login Details. Please Try Again..!");
 
         UserDTO userDTO = mapper.map(user, UserDTO.class);
+        CurrentUserUtil.currentUser = userDTO;
         return userDTO;
 
     }
