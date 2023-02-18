b77af49aac494c9c7eb9976e71d0d43e1c885615
Sandun Induranga <sandutharu40@gmail.com>
2023-02-18 20:33:15 +0530

    Manage Rent Controller Ajax Object Created

--- Frontend/controller/ManageRentController.js
+++ Frontend/controller/ManageRentController.js
@@ -2,3 +2,15 @@
  * @author : Sandun Induranga
  * @since : 0.1.0
  **/
+
+$.ajax({
+
+    url:baseurl+"rent/all",
+    method:"get",
+    contentType:"application/json",
+    dataType:"json",
+    success:function (res) {
+
+    }
+
+})
