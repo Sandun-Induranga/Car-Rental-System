c4b62fbf8e037702c79dada815675e3d529611ad
Sandun Induranga <sandutharu40@gmail.com>
2023-02-18 22:17:29 +0530

    Load All Rents On Frontend

--- Frontend/controller/ManageRentController.js
+++ Frontend/controller/ManageRentController.js
@@ -5,25 +5,56 @@
 
 $.ajax({
 
-    url:baseurl+"rent/all",
-    method:"get",
-    contentType:"application/json",
-    dataType:"json",
-    success:function (res) {
+    url: baseurl + "rent/all",
+    method: "get",
+    contentType: "application/json",
+    dataType: "json",
+    success: function (res) {
 
         for (let rent of res.data) {
 
             $("#rent-context").append(`
             <div class="card text-center p-2 w-50">
-            <div class="card-body">
+            <div class="card-body" id="${res.rentId}">
                 <h5 class="card-title">${rent.rentId}</h5>
                 <p class="card-text">Customer NIC : ${rent.nic.nic}</p>
                 <p class="card-text">Customer Name : ${rent.nic.name}</p>
-                <p class="card-text">Driver NIC : 123456</p>
-                <p class="card-text">Lost Damage Payment : Bank Receipt 123456</p>
-                <p class="card-text">Total Cost : 42000.00</p>
-                <p class="card-text">Pick Up Date : 21/02/2023 | Pick Up Time : 12:24</p>
-                <p class="card-text">Return Date : 25/02/2023 | Return Time : 12:24</p>
+                   <table class="table">
+                  <thead>
+                    <tr>
+                      <th scope="col">#</th>
+                      <th scope="col">Register Number</th>
+                      <th scope="col">Lost Damage Cost</th>
+                      <th scope="col">Car Cost</th>
+                      <th scope="col">Driver Cost</th>
+                      <th scope="col">Driver NIC</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+`);
+
+            for (let rentDetail of rent.rentDetails) {
+                $(`#${res.rentId}`).append(`
+                
+                    <tr>
+                      <th scope="row">1</th>
+                      <td>Mark</td>
+                      <td>Otto</td>
+                      <td>@mdo</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">2</th>
+                      <td>Jacob</td>
+                      <td>Thornton</td>
+                      <td>@fat</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">3</th>
+                      <td colspan="2">Larry the Bird</td>
+                      <td>@twitter</td>
+                    </tr>
+                  </tbody>
+                </table>
                 <section>
                     <button class="btn btn-success me-2">Accept</button>
                     <button class="btn btn-warning">Deny</button>
@@ -31,6 +62,7 @@ $.ajax({
             </div>
         </div>
         `);
+            }
 
         }
 
