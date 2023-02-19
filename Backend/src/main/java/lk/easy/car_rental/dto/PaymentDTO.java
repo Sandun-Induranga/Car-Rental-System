3355941a54b906d70a67fb5aae6c95324c0be9af
Sandun Induranga <sandutharu40@gmail.com>
2023-02-19 20:39:34 +0530

    PaymentDTO Created

---
+++ Backend/src/main/java/lk/easy/car_rental/dto/PaymentDTO.java
@@ -0,0 +1,34 @@
+package lk.easy.car_rental.dto;
+
+import lk.easy.car_rental.entity.Rent;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class PaymentDTO {
+
+    String paymentId;
+    Rent rentId;
+    String type;
+    String description;
+    LocalDate date;
+    LocalTime time;
+    BigDecimal total;
+    BigDecimal cash;
+    BigDecimal balance;
+
+}
