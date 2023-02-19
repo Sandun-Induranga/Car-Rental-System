082f813cc3780f6327eec73e8b735c70a1673dcb
Sandun Induranga <sandutharu40@gmail.com>
2023-02-19 21:11:38 +0530

    Mathod savePayment()

--- Backend/src/main/java/lk/easy/car_rental/service/impl/PaymentServiceImpl.java
+++ Backend/src/main/java/lk/easy/car_rental/service/impl/PaymentServiceImpl.java
@@ -1,9 +1,18 @@
 package lk.easy.car_rental.service.impl;
 
+import lk.easy.car_rental.dto.PaymentDTO;
+import lk.easy.car_rental.service.PaymentService;
+
 /**
  * @author : Sandun Induranga
  * @since : 0.1.0
  **/
 
-public class PaymentServiceImpl {
+public class PaymentServiceImpl implements PaymentService {
+
+    @Override
+    public void savePayment(PaymentDTO paymentDTO) throws RuntimeException {
+
+    }
+
 }
