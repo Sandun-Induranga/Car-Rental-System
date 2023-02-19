1c41e25b169fbb2fc03ab88149ffe741e0910f17
Sandun Induranga <sandutharu40@gmail.com>
2023-02-19 21:04:30 +0530

    Method savePayment()

--- Backend/src/main/java/lk/easy/car_rental/controller/ManagePaymentController.java
+++ Backend/src/main/java/lk/easy/car_rental/controller/ManagePaymentController.java
@@ -1,6 +1,8 @@
 package lk.easy.car_rental.controller;
 
+import lk.easy.car_rental.util.ResponseUtil;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/payment")
 @CrossOrigin
 public class ManagePaymentController {
+
+    @PostMapping
+    public ResponseUtil savePayment() {
+        System.out.println("Invoked");
+        return null;
+    }
+
 }
