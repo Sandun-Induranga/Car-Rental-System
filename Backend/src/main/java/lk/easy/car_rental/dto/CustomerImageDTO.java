f527563496ec2177d40b43ebf4694b6c35dc21c9
Sandun Induranga <sandutharu40@gmail.com>
2023-02-22 12:06:18 +0530

    Customer ImageDTO Crated

---
+++ Backend/src/main/java/lk/easy/car_rental/dto/CustomerImageDTO.java
@@ -0,0 +1,24 @@
+package lk.easy.car_rental.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class CustomerImageDTO {
+
+    MultipartFile nicImage;
+    MultipartFile licenseImage;
+
+}
