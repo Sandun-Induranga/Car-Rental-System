a81b2bac5ec18fe93d54d4cdbdb194d380a0e52e
Sandun Induranga <sandutharu40@gmail.com>
2023-02-24 02:40:14 +0530

    Chart Added

--- Frontend/controller/ManageAdminController.js
+++ Frontend/controller/ManageAdminController.js
@@ -100,6 +100,36 @@ function manageHomePage() {
 
     });
 
+    var options = {
+        animationEnabled: true,
+        title: {
+            text: "GDP Growth Rate - 2016"
+        },
+        axisY: {
+            title: "Growth Rate (in %)",
+            suffix: "%"
+        },
+        axisX: {
+            title: "Countries"
+        },
+        data: [{
+            type: "column",
+            yValueFormatString: "#,##0.0#"%"",
+            dataPoints: [
+                { label: "Iraq", y: 10.09 },
+                { label: "Turks & Caicos Islands", y: 9.40 },
+                { label: "Nauru", y: 8.50 },
+                { label: "Ethiopia", y: 7.96 },
+                { label: "Uzbekistan", y: 7.80 },
+                { label: "Nepal", y: 7.56 },
+                { label: "Iceland", y: 7.20 },
+                { label: "India", y: 7.1 }
+
+            ]
+        }]
+    };
+    $("#chartContainer").CanvasJSChart(options);
+
 }
 
 
