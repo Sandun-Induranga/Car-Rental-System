6d94fea28a8ce256643363d50961883313eb5bb8
Sandun Induranga <sandutharu40@gmail.com>
2023-02-24 00:02:53 +0530

    Manage Home Page

--- Frontend/controller/ManageAdminController.js
+++ Frontend/controller/ManageAdminController.js
@@ -45,6 +45,24 @@ manageCarPage();
 manageDriverPage();
 manageRentPage();
 managePaymentsPage();
+manageHomePage();
+
+function manageHomePage() {
+
+    $("#btnHome").on("click", function () {
+        $('#home').fadeIn();
+        $("#home").attr("style", "display : block !important");
+        $("#viewCustomer").attr("style", "display : none !important");
+        $("#manageCustomers").attr("style", "display : none !important");
+        $("#manageCar").attr("style", "display : none !important");
+        $("#viewCar").attr("style", "display : none !important");
+        $("#manageDriver").attr("style", "display : none !important");
+        $("#drivers").attr("style", "display : none !important");
+        $("#rents").attr("style", "display : none !important");
+        $("#payments").attr("style", "display : none !important");
+    })
+
+}
 
 
 function manageCustomerPage() {
