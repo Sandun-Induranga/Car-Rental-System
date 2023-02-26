c188612f8a3d388405e87edd73b1b4365116b218
Sandun Induranga <sandutharu40@gmail.com>
2023-02-26 10:38:25 +0530

    Created Controller Manage Home Page

---
+++ Frontend/controller/ManageHomeController.js
@@ -0,0 +1,71 @@
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+
+function loadAllCars() {
+
+    $.ajax({
+        url: baseurl + "car",
+        method: "get",
+
+        success: function (res) {
+            $("#cars").empty();
+            for (let car of res.data) {
+                $("#cars").append(`<div class="col col-lg-4">
+            <div class="card border border-0 pt-2">
+                <img src="../assets/${car.photos.front}" class="card-img-top" alt="car">
+
+                <div class="card-body">
+                    <h5 class="card-title">${car.brand}</h5>
+
+                    <section class="mb-4">
+                        <img src="../assets/${car.photos.back}" class="w-25" alt="${car.photos.back}">
+                        <img src="../assets/${car.photos.side}" class="w-25" alt="car">
+                        <img src="../assets/${car.photos.interior}" class="w-25" alt="car">
+                    </section>
+
+                    <section class="d-flex gap-3 justify-content-between">
+                        <p class="card-text"><i class="bi bi-fuel-pump-diesel-fill me-1 text-success"></i>${car.fuelType}</p>
+                        <p class="card-text"><i class="bi bi-palette-fill me-1 text-danger"></i>${car.color}</p>
+                        <p class="card-text"><i class="bi bi-gear-wide-connected me-1 text-info"></i>${car.transmissionType}</p>
+                        <p class="card-text"><i class="bi bi-people-fill me-1 text-primary"></i>${car.passengers}</p>
+                    </section>
+
+                    <section class="row justify-content-between">
+                        <p class="card-text col col-md-5">Free Mileage</p>
+                        <p class="card-text text-secondary col col-lg-3 mb-lg-0 mb-4">${car.freeMileage.dailyRate}km Daily</p>
+                        <p class="card-text text-secondary col col-lg-3 mb-lg-0 mb-4">${car.freeMileage.monthlyRate}km Monthly</p>
+                    </section>
+
+                    <section class="row justify-content-between">
+                        <p class="card-text col col-4">Price</p>
+                        <p class="card-text text-secondary col col-lg-4 mb-lg-0 mb-4">${car.price.dailyPriceRate} LKR Daily</p>
+                        <p class="card-text text-secondary col col-lg-4 mb-lg-0 mb-4">${car.price.monthlyPriceRate} LKR Monthly</p>
+                    </section>
+
+                    <section class="row justify-content-between">
+                        <p class="card-text col col-lg-4">Lost Damage Cost</p>
+                        <p class="card-text text-secondary col">${car.lostDamageCost} LKR</p>
+                    </section>
+
+                    <section class="row justify-content-between">
+                        <p class="card-text text-secondary col col-6" id="registerNum"><i class="bi bi-car-front me-1"></i>${car.regNum}</p>
+                        <p class="card-text text-secondary col col-6 text-danger">${car.availability == "YES" ? "" : "Out Of Stock"}</p>
+                    </section>
+
+                    <section class="d-flex justify-content-between flex-lg-row flex-column gap-1">
+                        <button class="btn btn-success rent" data-bs-toggle="modal" data-bs-target="#staticBackdrop"><p class="card-text"><i class="bi bi-upc-scan"></i> Login to Rent </p></button>
+                    </section>
+
+                </div>
+
+            </div>
+        </div>`);
+            }
+
+        }
+
+    });
+
+}
