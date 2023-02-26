b806cfe2dac6a0e81b8381155a45dfa7073b583c
Sandun Induranga <sandutharu40@gmail.com>
2023-02-26 21:06:57 +0530

    Daily Sales

--- Backend/src/main/java/lk/easy/car_rental/repo/PaymentRepo.java
+++ Backend/src/main/java/lk/easy/car_rental/repo/PaymentRepo.java
@@ -2,6 +2,7 @@ package lk.easy.car_rental.repo;
 
 import lk.easy.car_rental.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -13,5 +14,10 @@ import java.util.List;
 public interface PaymentRepo extends JpaRepository<Payment, Integer> {
 
     List<Payment> findAllByRentId_Nic_Nic(String nic) throws RuntimeException;
+
+//    SELECT `date`, SUM(total) FROM Payment GROUP BY `date` HAVING `date`+7>= DATE(now())
+
+    @Query(value = "SELECT `date`, SUM(total) FROM Payment GROUP BY `date`",nativeQuery = true)
+    List<Payment> getDailyIncome() throws RuntimeException;
 
 }
