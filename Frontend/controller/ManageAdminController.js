345cef0f9f066cdfcc71bf485700baf98406222b
Sandun Induranga <sandutharu40@gmail.com>
2023-02-27 17:24:53 +0530

    Content Updated

--- Frontend/controller/ManageAdminController.js
+++ Frontend/controller/ManageAdminController.js
@@ -341,7 +341,7 @@ function manageCustomerPage() {
 
         // customer regular expressions
         const cusNameRegEx = /^[A-z ]{5,20}$/;
-        const cusEmailRegEx = /^[a-z ]{5,20}@[a-z]{1,}[.com]$/;
+        const cusEmailRegEx = /^[a-z ]{5,20}@[a-z][gmail.com]$/;
         const cusNicRegEx = /^[0-9]{9,10}[A-z]?$/;
         const cusAddressRegEx = /^[0-9/A-z. ,]{5,}$/;
         const cusContactRegEx = /^[0-9]{10}$/;
@@ -1048,51 +1048,42 @@ function manageReports() {
         $("#payments").attr("style", "display : none !important");
         $("#reports").attr("style", "display : block !important");
 
-        // var dataPoints = [];
-        //
-        // var options = {
-        //     title: {
-        //         text: "Desktop OS Market Share in 2017"
-        //     },
-        //     subtitles: [{
-        //         text: "As of November, 2017"
-        //     }],
-        //     animationEnabled: true,
-        //     data: [{
-        //         type: "pie",
-        //         startAngle: 40,
-        //         toolTipContent: "<b>{label}</b>: {y}%",
-        //         showInLegend: "true",
-        //         legendText: "{label}",
-        //         indexLabelFontSize: 16,
-        //         indexLabel: "{label} - {y}%",
-        //         dataPoints: [
-        //             { y: 48.36, label: "Windows 7" },
-        //             { y: 26.85, label: "Windows 10" },
-        //             { y: 1.49, label: "Windows 8" },
-        //             { y: 6.98, label: "Windows XP" },
-        //             { y: 6.53, label: "Windows 8.1" },
-        //             { y: 2.45, label: "Linux" },
-        //             { y: 3.32, label: "Mac OS X 10.12" },
-        //             { y: 4.03, label: "Others" }
-        //         ]
-        //     }]
-        // };
-        // $("#chartContainer").CanvasJSChart(options);
-        //
-        // $.ajax({
-        //     url: baseurl + "payment/daily",
-        //     method:"get",
-        //     success:function (res) {
-        //         for (var i = 0; i < res.data.length; i++) {
-        //             dataPoints.push({
-        //                 x: new Date(res.data[i][0]),
-        //                 y: res.data[i][1]
-        //             });
-        //         }
-        //         $("#chart").CanvasJSChart(options);
-        //     }
-        // });
+        var dataPoints = [];
+
+        var options = {
+            title: {
+                text: "Desktop OS Market Share in 2017"
+            },
+            subtitles: [{
+                text: "As of November, 2017"
+            }],
+            animationEnabled: true,
+            data: [{
+                type: "pie",
+                startAngle: 40,
+                toolTipContent: "<b>{label}</b>: {y}%",
+                showInLegend: "true",
+                legendText: "{label}",
+                indexLabelFontSize: 16,
+                indexLabel: "{label} - {y}%",
+                dataPoints: dataPoints
+            }]
+        };
+        $("#chartContainer").CanvasJSChart(options);
+
+        $.ajax({
+            url: baseurl + "payment/daily",
+            method:"get",
+            success:function (res) {
+                for (var i = 0; i < res.data.length; i++) {
+                    dataPoints.push({
+                        x: new Date(res.data[i][0]),
+                        y: res.data[i][1]
+                    });
+                }
+                $("#chart").CanvasJSChart(options);
+            }
+        });
 
 
         // $.getJSON("https://canvasjs.com/data/gallery/jquery/daily-sales-data.json", addData);
